9881ebe4a65e40dbbf09be9ff84cbe1b35e4c883
Adam Boas <anboas@gmail.com>
2014-10-30 10:37:59 -0400

    Initial Commit of new challenge:
    https://www.codeeval.com/open_challenges/24/

---
+++ CodeEval/Easy/sumOfIntegers/Main.java
@@ -0,0 +1,25 @@
+package sumOfIntegers;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		File file = new File(args[0]);
+		//File file = new File("E:\\Users\\whspr\\git\\CodeEval\\CodeEval\\Easy\\sumOfIntegers\\input.txt");
+		BufferedReader in = new BufferedReader(new FileReader(file));
+		String line;
+		String[] words;
+		int sumOfIntegers = 0;
+
+		while ((line = in.readLine()) != null) {
+			line = line.trim();
+			sumOfIntegers += Integer.parseInt(line);
+		}
+		System.out.println(sumOfIntegers);
+		in.close();
+	}
+}
